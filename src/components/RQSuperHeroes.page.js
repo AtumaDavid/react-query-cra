879c95afa51e69c649ee887f103ed7376f8773e5
Atuma David <atumadavid35@gmail.com>
2024-09-03 01:37:20 +0100

    react query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,15 +6,45 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const onSuccess = (data) => {
+    console.log("perform side effect after data fetching", data);
+  };
 
-  if (isLoading) {
+  const onError = (error) => {
+    console.log("perform side effect after encountering error", error);
+  };
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    {
+      onSuccess: onSuccess,
+      onError,
+    },
+    {
+      // cacheTime: 10000,
+      // staleTime: 30000,
+      // refetchOnMount: true,
+      // refetchOnWindowFocus: true, default
+      // refetchInterval: 2000 polling(2sec)
+      // refetchIntervalInBackground: true
+      // enabled: false,
+    }
+  );
+
+  console.log(`loading: ${isLoading}`, `fetching: ${isFetching}`);
+
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Error: {error.message}</h2>;
   }
 
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+      {/* <button onClick={refetch}>Fetch heros</button> //enabled */}
       {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
